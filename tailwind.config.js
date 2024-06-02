d4887200e398c9a37d3055253b6e0591e3dcc7dc
Prasun Mondal <prasun0403.pm@gmail.com>
2024-06-02 23:05:34 +0530

    no update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./*/*.{liquid,json,js}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1.25rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "5rem",
+      },
+      screens: {
+        xxm: '360px',
+        sm: '640px',
+        md: '750px',
+        lg: '1024px',
+        xl: '1440px',
+        x2lg: '1920px',
+      }
+    },
     extend: {},
   },
   plugins: [],
