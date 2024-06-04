7f478e034178e941e6404e72d230971e2ac503d5
Prasun Mondal <prasun0403.pm@gmail.com>
2024-06-04 23:42:31 +0530

    link list added to header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,17 @@ module.exports = {
   content: ["./*/*.{liquid,json,js}"],
   theme: {
     container: {
+      center: true,
       padding: {
         DEFAULT: "1.25rem",
         sm: "2rem",
-        lg: "3rem",
-        xl: "4rem",
-        "2xl": "5rem",
+        lg: "2.5rem",
+        xl: "3rem",
+        "2xl": "4rem",
       },
       screens: {
         DEFAULT: '100%',
-        sm: '100%',
-        md: '750px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
+        
       }
     },
     extend: {},
